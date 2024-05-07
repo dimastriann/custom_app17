7e532e984bfcc2fa2416829e36f38a692da218ec
dimastriann <dimastriannugraha@gmail.com>
2024-05-07 11:41:31 +0700

    [UPDATE] update code

---
+++ training_training/models/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class training_training(models.Model):
+#     _name = 'training_training.training_training'
+#     _description = 'training_training.training_training'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
